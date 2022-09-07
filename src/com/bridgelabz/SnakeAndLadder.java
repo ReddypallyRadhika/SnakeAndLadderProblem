49a5816f81f10df37b58556e87d27d69efa77772
Radhika <reddypallyradhika@gmail.com>
2022-09-07 19:54:19 +0530

    Added: Snake and Ladder: UC1

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,6 +3,8 @@
  */
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 /**
  * @author hp
  *
@@ -12,9 +14,17 @@ public class SnakeAndLadder {
 	/**
 	 * @param args
 	 */
+	/*
+	 * UC1
+	 * Snake and Ladder game played with single player at start position 0
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the name of the player:");
+        String player= sc.next();
+        int playerposition=0;
+        System.out.println("player position:"+playerposition);
 	}
 
 }
