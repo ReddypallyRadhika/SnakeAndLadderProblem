4a822efb50fffa0fedcebe94a4db65b810b46675
Radhika <reddypallyradhika@gmail.com>
2022-09-07 19:38:39 +0530

    Welcome To Snake & Ladder Simulator

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author hp
+ *
+ */
+public class SnakeAndLadder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/readme.md
@@ -0,0 +1,3 @@
+Snake & Ladder
+
+Simulator
