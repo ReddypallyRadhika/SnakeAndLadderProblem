ec828fec2b281b82fbc2a752a1282ae45b318bca
Radhika <reddypallyradhika@gmail.com>
2022-09-07 20:15:52 +0530

    Added: Snake and Ladder : UC2

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -18,6 +18,10 @@ public class SnakeAndLadder {
 	 * UC1
 	 * Snake and Ladder game played with single player at start position 0
 	 */
+	/*UC2
+	 * The Player rolls the die to get a number between 1 to 6.
+	 * - Use ((RANDOM)) to get the number between 1 to 6
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
@@ -25,6 +29,8 @@ public class SnakeAndLadder {
         String player= sc.next();
         int playerposition=0;
         System.out.println("player position:"+playerposition);
+        int rolldie = (int) (Math.floor(Math.random()*10)%7);
+        System.out.println("player rolldie:"+rolldie);
 	}
 
 }
