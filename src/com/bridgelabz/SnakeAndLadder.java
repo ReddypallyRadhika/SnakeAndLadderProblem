dfe325e475a807db9f7bf59404937a9a9611b40e
Radhika <reddypallyradhika@gmail.com>
2022-09-21 15:17:15 +0530

    Added:UC7:Snake and Ladder Problem

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -11,94 +11,103 @@ import java.util.Scanner;
  */
 public class SnakeAndLadder {
 
-	/**
-	 * @param args
-	 */
 	/*
-	 * UC1
-	 * Snake and Ladder game played with single player at start position 0
-	 */
-	/*UC2
-	 * The Player rolls the die to get a number between 1 to 6.
-	 * - Use ((RANDOM)) to get the number between 1 to 6
-	 */
-	/*UC3
-	 * The Player then checks for a Option. They are No Play, Ladder or Snake.
-	 */
-	/*UC4
-	 * Repeat till the Player reaches the winning position 100. 
-	 * - Note In case the player position moves below 0,
-	 *  then the player restarts from 0
-	 * 
-	 */
-	/*
-	 * UC5
-	 * Ensure the player gets to exact winning position 100.
-	 *  - Note in case the player position go above 100, 
-	 *  the player stays in the same previous position till the player gets the exact number that adds to 100
-	 */
-	/*
-	 * UC6
-	 * Report the number of times the dice was played to win the game and also the position after every die role
+	 * UC7 Play the game with 2 Player. In this case if a Player gets a Ladder then
+	 * plays again. Finally report which Player won the game
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the name of the player:");
-        String player= sc.next();
-        int playerposition=0;
-        int playernewposition = 0;
-        int winningposition = 100;
-        int count= 0;
-        System.out.println("player position:"+playerposition);
-        int rolldie = (int) (Math.floor(Math.random()*10)%7);
-        while(playernewposition<1 || playernewposition!=winningposition)
-       {
-        	
-    	  System.out.println("restart game");
-        for(playerposition=0;playerposition<=100;playerposition=rolldie+playernewposition)
-        {
-        	
-        	
-        	int randomOption =(int) (Math.floor(Math.random()*10)%4);
-        
-        	System.out.println(randomOption);
-        if(randomOption ==1)
-        {
-        	 System.out.println("Ladder");
-        	 playernewposition=rolldie+playernewposition;
-        	 System.out.println("player position:"+playernewposition);
-        }
-        else if(randomOption ==2)
-            {
-            	 System.out.println("Snake");
-            	 playernewposition=playernewposition-rolldie;
-            	 System.out.println("player position:"+playernewposition);
-            }
-       else
-        {
-        	System.out.println("No Play");
-        	 System.out.println("player position:"+playernewposition);
-	     }
-        
-        
-       
-        
-        if(playernewposition>=winningposition)
-    	{
-        	System.out.println("player position:"+playernewposition);
-        	System.out.println("Stop");
-        }
-        ++count;
-	    
-	  
-        }
-    
-		  
-     }
-        System.out.println("Total Number Of Times Die Rolled: " + count);
-      
-	}   
-	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name of the player1:");
+		String player1 = sc.next();
+		System.out.println("Enter the name of the player2:");
+		String player2 = sc.next();
+		int playerposition = 0;
+		int playernewposition = 0;
+		int winningposition = 100;
+		int count = 0;
+
+		System.out.println("player position:" + playerposition);
+		int rolldie = (int) (Math.floor(Math.random() * 10) % 7);
+		if (playerposition == 0 || playernewposition == 0) {
+			switch (rolldie) {
+			case 1:
+				while (playernewposition < 1 || playernewposition != winningposition) {
+
+					System.out.println("restart game");
+					for (playerposition = 0; playerposition <= 100; playerposition = rolldie + playernewposition) {
+
+						int randomOption = (int) (Math.floor(Math.random() * 10) % 4);
+
+						System.out.println(randomOption);
+						if (randomOption == 1) {
+							System.out.println("Ladder");
+							playernewposition = rolldie + playernewposition;
+							System.out.println("player position:" + playernewposition);
+						} else if (randomOption == 2) {
+							System.out.println("Snake");
+							playernewposition = playernewposition - rolldie;
+							System.out.println("player position:" + playernewposition);
+						} else {
+							System.out.println("No Play");
+							System.out.println("player position:" + playernewposition);
+						}
+
+						if (playernewposition >= winningposition) {
+							System.out.println("player position:" + playernewposition);
+							System.out.println("Stop");
+						}
+						++count;
+
+					}
+
+					if (playernewposition == winningposition) {
+						System.out.println("player1 won the game");
+					}
+
+				}
+				System.out.println("Total Number Of Times Die Rolled: " + count);
+
+				break;
+			case 2:
+
+				while (playernewposition < 1 || playernewposition != winningposition) {
+
+					System.out.println("restart game");
+					for (playerposition = 0; playerposition <= 100; playerposition = rolldie + playernewposition) {
+
+						int randomOption = (int) (Math.floor(Math.random() * 10) % 4);
+
+						System.out.println(randomOption);
+						if (randomOption == 1) {
+							System.out.println("Ladder");
+							playernewposition = rolldie + playernewposition;
+							System.out.println("player position:" + playernewposition);
+						} else if (randomOption == 2) {
+							System.out.println("Snake");
+							playernewposition = playernewposition - rolldie;
+							System.out.println("player position:" + playernewposition);
+						} else {
+							System.out.println("No Play");
+							System.out.println("player position:" + playernewposition);
+						}
+
+						if (playernewposition >= winningposition) {
+							System.out.println("player position:" + playernewposition);
+							System.out.println("Stop");
+						}
+						++count;
+
+					}
+
+					if (playernewposition == winningposition) {
+						System.out.println("player2 won the game");
+					}
+
+				}
+				System.out.println("Total Number Of Times Die Rolled: " + count);
+
+			}
+		}
+	}
 
 }
